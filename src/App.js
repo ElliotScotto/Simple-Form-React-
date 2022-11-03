39db25264d6d5112720265c1134619186dfbec01
Elliot Scotto <112620044+ElliotScotto@users.noreply.github.com>
2022-11-03 16:55:04 +0100

    component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import "./App.css";
 import { useState } from "react";
+import StepTwo from "./Components/StepTwo";
 
 function App() {
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmpassword] = useState("");
   return (
     <>
       <main>
         <header>Create account</header>
-        <form>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            if (password !== confirmpassword) {
+              alert("Vos 2 mots de passe ne sont pas identiques");
+            } else {
+            }
+          }}
+        >
           <span className="title">Name</span>
           <input type="text" name="name" placeholder="Elliot Scotto"></input>
           <span className="title">Email</span>
@@ -18,19 +29,30 @@ function App() {
           ></input>
           <span className="title">Password</span>
           <input
+            value={password}
             type="password"
             name="password"
             placeholder="IErEaCtEuR2020"
+            onChange={(evntpassword) => {
+              setPassword(evntpassword.target.value);
+            }}
           ></input>
           <span className="title">Confirm Your password</span>
           <input
+            value={confirmpassword}
             type="password"
             name="confirmpassword"
-            placeholder="IErEaCtEuR2020"
+            onChange={(evntconfirmpassword) => {
+              setConfirmpassword(evntconfirmpassword.target.value);
+            }}
           ></input>
-          <button type="submit" name="register" onClick={() => {}}>
+
+          <button type="submit" name="register">
             Register
           </button>
+          {/* {evntpassword.target.value} === {evntconfirmpassword.target.value} ?
+          {alert("Vos 2 mots de passe correspondent !")} :
+          {alert("vos 2 mots de passe ne correspondent pas.")} */}
         </form>
         <footer>
           Made with<span> React </span>at<span> Le Reacteur </span>by
---
+++ src/Components/StepTwo.js
@@ -0,0 +1,4 @@
+const StepTwo = () => {
+  return <p>coucou</p>;
+};
+export default StepTwo;
