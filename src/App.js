71a949bb1ba16cd0eb397497f880c198d242fddf
Elliot Scotto <112620044+ElliotScotto@users.noreply.github.com>
2022-11-03 18:29:01 +0100

    coucou

--- src/App.js
+++ src/App.js
@@ -3,57 +3,70 @@ import { useState } from "react";
 import StepTwo from "./Components/StepTwo";
 
 function App() {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmpassword, setConfirmpassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isValid, setIsValid] = useState(false);
+
   return (
     <>
       <main>
-        <header>Create account</header>
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            if (password !== confirmpassword) {
-              alert("Vos 2 mots de passe ne sont pas identiques");
-            } else {
-            }
-          }}
-        >
-          <span className="title">Name</span>
-          <input type="text" name="name" placeholder="Elliot Scotto"></input>
-          <span className="title">Email</span>
-          <input
-            type="email"
-            name="email"
-            placeholder="elliot.scotto@gmail.com"
-          ></input>
-          <span className="title">Password</span>
-          <input
-            value={password}
-            type="password"
-            name="password"
-            placeholder="IErEaCtEuR2020"
-            onChange={(evntpassword) => {
-              setPassword(evntpassword.target.value);
-            }}
-          ></input>
-          <span className="title">Confirm Your password</span>
-          <input
-            value={confirmpassword}
-            type="password"
-            name="confirmpassword"
-            onChange={(evntconfirmpassword) => {
-              setConfirmpassword(evntconfirmpassword.target.value);
-            }}
-          ></input>
+        {isValid ? (
+          <StepTwo />
+        ) : (
+          <>
+            <header>Create account</header>
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+                if (password !== confirmPassword) {
+                  alert("Vos 2 mots de passe ne sont pas identiques");
+                } else {
+                  // afficher stepTwo
+                  setIsValid(true);
+                }
+              }}
+            >
+              <span className="title">Name</span>
+              <input
+                type="text"
+                name="name"
+                placeholder="Elliot Scotto"
+              ></input>
+              <span className="title">Email</span>
+              <input
+                type="email"
+                name="email"
+                placeholder="elliot.scotto@gmail.com"
+              ></input>
+              <span className="title">Password</span>
+              <input
+                value={password}
+                type="password"
+                name="password"
+                placeholder="IErEaCtEuR2020"
+                onChange={(evntpassword) => {
+                  setPassword(evntpassword.target.value);
+                }}
+              ></input>
+              <span className="title">Confirm Your password</span>
+              <input
+                value={confirmPassword}
+                type="password"
+                name="confirmpassword"
+                onChange={(evntconfirmPassword) => {
+                  setConfirmPassword(evntconfirmPassword.target.value);
+                }}
+              ></input>
 
-          <button type="submit" name="register">
-            Register
-          </button>
-          {/* {evntpassword.target.value} === {evntconfirmpassword.target.value} ?
-          {alert("Vos 2 mots de passe correspondent !")} :
-          {alert("vos 2 mots de passe ne correspondent pas.")} */}
-        </form>
+              <button type="submit" name="register">
+                Register
+              </button>
+            </form>
+          </>
+        )}
+
         <footer>
           Made with<span> React </span>at<span> Le Reacteur </span>by
           <span> Elliot</span>
--- src/Components/StepTwo.js
+++ src/Components/StepTwo.js
@@ -1,4 +1,27 @@
-const StepTwo = () => {
-  return <p>coucou</p>;
+import { useState } from "react";
+
+const StepTwo = (props) => {
+  return (
+    <>
+      <header>Results</header>
+      <form className="stepTwo-style">
+        <div className="title">Name : {props.setName}</div>
+        <div className="title">Email : {props.setEmail}</div>
+        <div className="title">Password : {props.setPassword}</div>
+      </form>
+      <div
+        className="editBtn"
+        type="submit"
+        name="edit"
+        if
+        onSubmit={(event) => {
+          event.preventDefault();
+          //afficher le 1er formulaire avec infos persistantes.
+        }}
+      >
+        <p>Edit your information</p>
+      </div>
+    </>
+  );
 };
 export default StepTwo;
