1ec32a265eab9dc0209a2a099a7c664f81a87d26
Elliot Scotto <112620044+ElliotScotto@users.noreply.github.com>
2022-11-03 14:57:31 +0100

    structure and responsive mode OK

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [email, setEmail] = useState("");
+  return (
+    <>
+      <main>
+        <header>Create account</header>
+        <form>
+          <span className="title">Name</span>
+          <input type="text" name="name" placeholder="Elliot Scotto"></input>
+          <span className="title">Email</span>
+          <input
+            type="email"
+            name="email"
+            placeholder="elliot.scotto@gmail.com"
+          ></input>
+          <span className="title">Password</span>
+          <input
+            type="password"
+            name="password"
+            placeholder="IErEaCtEuR2020"
+          ></input>
+          <span className="title">Confirm Your password</span>
+          <input
+            type="password"
+            name="confirmpassword"
+            placeholder="IErEaCtEuR2020"
+          ></input>
+          <button type="submit" name="register" onClick={() => {}}>
+            Register
+          </button>
+        </form>
+        <footer>
+          Made with<span> React </span>at<span> Le Reacteur </span>by
+          <span> Elliot</span>
+        </footer>
+      </main>
+    </>
+  );
 }
 
 export default App;
